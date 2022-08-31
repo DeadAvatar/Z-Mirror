7e213863ee11c2f7041f89dc98cdf83175aea2dc
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2022-08-31 16:12:39 +0530

    Update ytdlp.py

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -252,11 +252,6 @@ def _auto_cancel(msg, msg_id):
     except:
         pass
 
-def ytdl(update, context):
-    _ytdl(context.bot, update.message)
-
-def ytdlZip(update, context):
-    _ytdl(context.bot, update.message, True)
 
 def ytdlleech(update, context):
     _ytdl(context.bot, update.message, isLeech=True)
@@ -264,18 +259,12 @@ def ytdlleech(update, context):
 def ytdlZipleech(update, context):
     _ytdl(context.bot, update.message, True, True)
 
-ytdl_handler = CommandHandler(BotCommands.YtdlCommand, ytdl,
-                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-ytdl_zip_handler = CommandHandler(BotCommands.YtdlZipCommand, ytdlZip,
-                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 ytdl_leech_handler = CommandHandler(BotCommands.YtdlLeechCommand, ytdlleech,
                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 ytdl_zip_leech_handler = CommandHandler(BotCommands.YtdlZipLeechCommand, ytdlZipleech,
                                     filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 quality_handler = CallbackQueryHandler(select_format, pattern="qu", run_async=True)
 
-dispatcher.add_handler(ytdl_handler)
-dispatcher.add_handler(ytdl_zip_handler)
 dispatcher.add_handler(ytdl_leech_handler)
 dispatcher.add_handler(ytdl_zip_leech_handler)
 dispatcher.add_handler(quality_handler)
